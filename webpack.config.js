214f305c2bd1c019608c176bc3a2f5406680484d
Jbee <Jbee@AL01155703.local>
2017-05-05 16:38:29 +0900

    Step-2. Add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: ['./src/index.js'],
@@ -17,9 +19,28 @@ module.exports = {
             }]
         }, {
             test: /\.css$/,
-            use: [
-                'style-loader', 'css-loader'
-            ]
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'css-loader'
+            })
         }]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoEmitOnErrorsPlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'commons',
+            filename: 'commons.js',
+            minChunks: Infinity
+        }),
+        new ExtractTextPlugin('styles.css'),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
     }
 };
